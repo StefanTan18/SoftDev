37af5203fb6260169a279b462242d98cb66b9f70
Stefan Tan <stan3@stuy.edu>
2018-10-01 21:02:17 -0400

    updated 14_login

--- 14_login/app.py
+++ 14_login/app.py
@@ -1,31 +1,43 @@
 '''
-Stefan Tan 
+Frozen Cookies - T. Fabiha && Stefan Tan
 SoftDev1 pd6
-K13 -- Echo Echo Echo
-2018-09-27
+K14 -- Do I Know You?
+2018-10-01
 '''
 
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, url_for, redirect
 import os 
 
 app= Flask(__name__) #create instance of class Flask
 
+username = "Bob"
+passwd = "bobby"
+
 @app.route("/")
 def hello_world():
-    if not session.get('logged_in'):
-        return render_template('login.html')
+    if session.get("username") == "Bob":
+        return render_template("auth.html", username = session.get("username"))
     else:
-        return render_template('auth.html')
+        return render_template("login.html")
 
-@app.route("/auth", methods=["POST"])
-def authenticate():
-    if request.form['password'] == 'password' and request.form['username'] == 'admin':
-        session['logged_in'] = True
+@app.route("/login", methods=["POST"])
+def login():
+    if request.form["username"] == username:
+        if request.form["password"] == passwd:
+            session["username"] = "Bob"
+            return render_template("auth.html", username = session.get("username"))
+        else:
+            return render_template("error.html", message = "Wrong Password. Please try again.")
     else:
-        flash('wrong password!')
-    return home()
+        return render_template("error.html", message = "Wrong Username. Please try again.")
+
+@app.route("/logout")
+def logout():
+    session.pop("username")
+    return redirect(url_for("hello_world"))
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(32)
     app.debug = True
     app.run()
+    
